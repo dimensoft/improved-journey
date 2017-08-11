d53c6822bca80e584a79e64f67cfb9e8110f33de
jason <jason@PC>
2017-08-11 15:50:30 +0800

    李名杰

---
+++ src/main/java/com/ilog/domainV2/AnsjAnalyzer.java
@@ -0,0 +1,23 @@
+
+
+package com.ilog.domain;
+
+import java.io.Reader;
+
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.TokenStream;
+
+public class AnsjAnalyzer extends Analyzer {
+
+	/**
+	 * Creates {@link org.apache.lucene.analysis.Analyzer.TokenStreamComponents}
+	 * used to tokenize all the text in the provided {@link Reader}.
+	 * 
+	 * @return {@link org.apache.lucene.analysis.Analyzer.TokenStreamComponents}
+	 *         built from a {@link RlsegTokenizer}
+	 */
+	protected TokenStreamComponents createComponents(String fieldName,
+			Reader reader) {
+		return new TokenStreamComponents(new AnsjTokenizer(reader,0,true));
+	}
+}
---
+++ src/main/java/com/ilog/domainV2/AnsjTokenizerFactory.java
@@ -0,0 +1,37 @@
+package com.ilog.domain;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.Reader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+
+import org.apache.lucene.analysis.Tokenizer;
+import org.apache.lucene.analysis.util.TokenizerFactory;
+import org.apache.lucene.util.AttributeFactory;
+
+
+public class  AnsjTokenizerFactory extends TokenizerFactory {
+
+	private int analysisType = 0;
+	private boolean rmPunc = true;
+	
+	
+	
+	public AnsjTokenizerFactory(Map<String, String> args) {
+		super(args);
+		assureMatchVersion();
+		analysisType = getInt(args, "analysisType", 0);
+		rmPunc = getBoolean(args, "rmPunc", true);
+	}
+
+
+		return new AnsjTokenizer(input, analysisType, rmPunc);
+	}
+
+	
+	
+}
